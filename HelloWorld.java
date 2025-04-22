8a7270a9ee789253ec04e98865fc93b388794de9
Evgenia <speaknarcissus@gmail.com>
2025-04-22 14:39:34 +0300

    first commit, hello world added

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * A program that prints <code>Hello World!</code>
+ *
+ * @author Nikita Astafyev
+ * @version 1.0
+ */
+public class HelloWorld {
+    /**
+     * A program that prints <code>Hello World!</code>
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
